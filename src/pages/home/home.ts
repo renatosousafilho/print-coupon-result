7a499d284ca80db8514eaaf07bc96715abe4bebb
Renato Filho <renatosousafilho@gmail.com>
2018-02-08 15:52:46 -0300

    imprimindo bilhete pelo botao do cupom

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -87,55 +87,80 @@ export class HomePage {
         var action = values[ 0 ];
         if ( action ) {
           if (action=="Print"){
-            browser.executeScript(
-              { code: "window.localStorage.setItem('action', '')" }
-            );
-            browser.executeScript({ code: "document.getElementsByClassName('printable')[0].innerHTML"})
-            .then(response => {
-              // alert("Printing...");
-              // alert(response);
-              let iframe=document.createElement('iframe');
-              document.body.appendChild(iframe);
-              var iframedoc=iframe.contentDocument||iframe.contentWindow.document;
-              var div=document.createElement('div');
-              div.classList.add("col-sm-12");
-              div.innerHTML=response;
-              iframedoc.body.appendChild(div);
-
-              var link=document.createElement('link');
-              link.href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css';
-              link.rel='stylesheet';
-              iframedoc.head.appendChild(link);
-
-              var css = 'body { width: 200px; }',
-              head = iframedoc.head,
-              style = document.createElement('style');
-
-              style.appendChild(document.createTextNode(css));
-
-              head.appendChild(style);
-              // $self.printCoupon(iframedoc);
-            });
+            $self.selectDevice(browser);
           }
           if (action=="Test") {
-            browser.executeScript(
-              { code: "window.localStorage.setItem('action', '')" }
-            );
-            $self.printImageProvider.listBluetoothDevices().then(datalist => {
-              browser.hide();
-              let modal=$self.modalCtrl.create(PrinterListModalPage,{data:datalist});
-              modal.present();
-              modal.onDidDismiss((data)=>{
-                browser.show();
-                $self.printImageProvider.connect(data.address).then(result => {
-                  $self.printImageProvider.printText("Hello");
-                  $self.printImageProvider.printText("Hello");
-                  $self.printImageProvider.printText("Hello");
-                });
-              });
-            });
+            $self.selectDevice(browser);
           }
         }
+      });
+    });
+  }
+
+  private selectDevice(browser){
+    var $self = this;
+    browser.executeScript(
+      { code: "window.localStorage.setItem('action', '')" }
+    );
+    $self.printImageProvider.listBluetoothDevices().then(datalist => {
+      browser.hide();
+      let modal=$self.modalCtrl.create(PrinterListModalPage,{data:datalist});
+      modal.present();
+      modal.onDidDismiss((data)=>{
+        browser.show();
+        $self.printImageProvider.connect(data.address).then(result => {
+          browser.executeScript(
+            { code: "window.localStorage.setItem('action', '')" }
+          );
+          browser.executeScript({ code: "document.getElementsByClassName('printable')[0].innerHTML"})
+          .then(response => {
+            // alert("Printing...");
+            // alert(response);
+            let iframe=document.createElement('iframe');
+            document.body.appendChild(iframe);
+            var iframedoc=iframe.contentDocument||iframe.contentWindow.document;
+            var div=document.createElement('div');
+            div.classList.add("col-sm-12");
+            div.innerHTML=response;
+            iframedoc.body.appendChild(div);
+
+            var link=document.createElement('link');
+            link.href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css';
+            link.rel='stylesheet';
+            iframedoc.head.appendChild(link);
+
+            var css = 'body { width: 200px; }',
+            head = iframedoc.head,
+            style = document.createElement('style');
+
+            style.appendChild(document.createTextNode(css));
+
+            head.appendChild(style);
+
+            setTimeout(function(){
+                var imagedata;
+                html2canvas(iframedoc.body)
+                .then(function(canvas) {
+                  imagedata = canvas.toDataURL('image/png');
+                  var myImage = new Image();
+                  myImage.src = imagedata;
+                  myImage.onload = function () {
+                    var canvas = document.createElement("canvas");
+                    canvas.height = 210;
+                    canvas.width = 382;
+                    var context = canvas.getContext('2d');
+                    context.drawImage(myImage, 0, 0);
+                    var imageBase =
+                    canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg|jpeg);base64,/,"");
+                    $self.printImageProvider.connect(data.address).then(result => {
+                      $self.printImageProvider.printImage(imageBase, canvas.width, canvas.height, 0);
+                    });
+                  };
+                })
+                .catch((error) => {alert('erro no html2canvas: ' + error)});
+            }, 10);
+          });
+        });
       });
     });
   }
